9a4635bb95742f84bcd4b42d59be6469f55da24e
gabipirillo <gpmanoel123@gmail.com>
2024-05-15 09:39:03 -0300

    Futebol

--- js/apagar.js
+++ js/apagar.js
@@ -1,10 +1,13 @@
 function apagar(id){
     // Pegar os jogadores do localStorage
-
+    const jogadores = JSON.parse(localStorage.getItem("jogadores")) || []
     // Filtrar os jogadores para remover o jogador com o id passado como parâmetro
-
+    const jogadoresFiltro = jogadores.filter(jogador => jogador.id != id)
     // Salvar a lista de jogadores no localStorage
-
+    localStorage.setItem("jogadores", JSON.stringify(jogadoresFiltro))
     // Remover o item da página ou recarregar a página
+    window.location.reload()
+    document.querySelector(id).remove()
+
 
 }
--- js/cadastrar.js
+++ js/cadastrar.js
@@ -1,17 +1,24 @@
 document.querySelector("#botao-cadastrar").addEventListener("click", () => {
     // Pegar os valores dos inputs
-
     // Criar um objeto chamado jogador com esses valores
     const jogador = {
         id: new Date().getTime(),
-        nome: "Ronaldo",
-        time: "Real Madrid",
-        gols: 100,
-        assistencias: 3,
-        foto: "https://cdn.footystats.org/img/players/portugal-cristiano-ronaldo.png"
+        nome: document.querySelector("#nome").value,
+        time: document.querySelector("#time").value,
+        gols: document.querySelector("#gols").value,
+        assistencias: document.querySelector("#assistencias").value,
+        foto: document.querySelector("#foto").value
     }
 
     // Salvar o objeto no localStorage usando a chave "jogadores"
-
+    salvar(jogador)
     // Redirecionar o usuário para a página inicial
 })
+
+function salvar(jogador){
+    const jogadores = JSON.parse(localStorage.getItem("jogadores")) || []
+    jogadores.push(jogador)
+    localStorage.setItem("jogadores", JSON.stringify(jogadores))
+    window.location.href = "./../index.html"
+
+}
